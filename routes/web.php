7383c39c311ae56707599a4a17290223c8659006
Md.Safaet <65019316+mdsafaet@users.noreply.github.com>
2025-05-25 11:44:40 +0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -25,10 +25,28 @@ Route::get('/suppliers/home', function () {
     return view('supplier_home');
 });
 
+// Safaet
+Route::get('/view-roster', function () {
+    return view('ViewRoster');
+});
+Route::get('/manage-team', function () {
+    return view('ManageTeam');
+});
+Route::get('/staff-attendance', function () {
+    return view('Staff-Attendance');
+});
+
+Route::get('/pantry-list', function () {
+    return view('PantryList');
+});
+
+//safaet
 
 
 
 
+//fardin
 Route::view('/dashboard', 'pages.dashboard');
+//fardin
 
 
